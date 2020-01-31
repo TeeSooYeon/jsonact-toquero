3a0a44d1bc958dcdfd0a3a995aa826c39dfd217b
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-01-31 20:04:08 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,10 +1,10 @@
 <?php 
-include 'config.php';
-include 'class.manage.php';
+include 'dbconnect.php';
+include 'class.retrieve.php';
 header('Content-Type: application/json');
-$manage = new Manage();
-$jets = $manage->get_jets();
-echo "{\"jets\":";
-echo json_encode($jets);
+$manage = new Get();
+$virus = $manage->virus_retrv();
+echo "{\"Virus\":";
+echo json_encode($virus);
 echo "}";
 ?>
